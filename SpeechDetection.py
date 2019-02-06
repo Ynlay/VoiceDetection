5db4f1675e3d2bfc9198f8071ce706fa125a9cb1
Ynlay <aronisfotis@gmail.com>
2019-02-06 22:55:01 +0200

    update

--- SpeechDetection.py
+++ SpeechDetection.py
@@ -1,23 +1,22 @@
 import os
 import speech_recognition as sr
-import CameraFaceDetection as cd
-import ModernArt as ma
+# import CameraFaceDetection as cd
 from time import sleep 
-from docx import Document
 from gtts import gTTS
-from mutagen.mp3 import MP3
 
 # Obtain audio from microphone
 r = sr.Recognizer()
-
+mic = sr.Microphone()
 
 # Listens 
-def listenSource():
-    print("Say Something!")
-    with sr.Microphone() as source:
+def listenSource(): 
+    with mic as source:
+        print("Say Something!") 
+        r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-    print("Got it!")
-    return audio
+        audio = r.recognize_google(audio, language="en-US")
+        #audio = r.recognize_google(audio, language="el-GR")
+        return audio
 
 
 # Speaks
@@ -26,23 +25,15 @@ def speak(textToSpeech):
     tts = gTTS(textToSpeech, lang='en')  # gTTS requires internet connection...
     tts.save("textToSpeech.mp3")    
     os.startfile("textToSpeech.mp3")  # Need to improve on that. maybe mpg321!
-    mp3Length = MP3("textToSpeech.mp3").info.length
-    sleep(mp3Length)  # In order to avoid listening to itself.
-
 
 # Will be adding all the different functionalities here
 def functionalities(voiceInput):
     print(voiceInput)
     print("casefold: " + voiceInput.casefold())
-    if (voiceInput == "Hello Athena".casefold()):
+
+    if (voiceInput == "Hello".casefold()):
         speak("Greetings!")
         print("Greetings!")
-
-    if (voiceInput == "Face Detection".casefold()):
-        cd.CameraDetection("haarcascade_frontalface_default.xml")
-
-    if ("Draw".casefold() in voiceInput.casefold()):
-        ma.Draw()
 
 
 # Recognize speech using Google Speech Recognition
@@ -52,13 +43,14 @@ try:
     # 'r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")
     # instead of 'r.recognize_google(audio)'
     while(True):
-        voiceInput = r.recognize_google(listenSource())
+        voiceInput = listenSource()
         print("Google Speech Recognition thinks you said: " + voiceInput)
-        speak("I think you just said: " + voiceInput)
+        speak("You Said: " + voiceInput)
         
         # Exit the loop if the command was exit 
         if ("exit" in voiceInput): 
             print("Exiting...")
+            speak("Thanks for your cooperation human, I am shutting down now...")
             break
 
         # Execute functionalities
